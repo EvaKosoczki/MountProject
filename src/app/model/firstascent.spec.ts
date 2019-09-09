e42f7d377ac00e9e56411365a34aec743b65dccd
Kosoczki Eva <eva.kosoczki@gmail.com>
2019-09-09 18:22:00 +0200

    firstascent raw data

---
+++ src/app/model/firstascent.spec.ts
@@ -0,0 +1,7 @@
+import { Firstascent } from './firstascent';
+
+describe('Firstascent', () => {
+  it('should create an instance', () => {
+    expect(new Firstascent()).toBeTruthy();
+  });
+});
---
+++ src/app/model/firstascent.ts
@@ -0,0 +1,11 @@
+export class Firstascent {
+  Id: Number,
+  Date: string;
+  Peak: string;
+  Height: number;
+  Range: string;
+  Climbers: string;
+  Ref: string;
+  FIELD8: string;
+
+}
